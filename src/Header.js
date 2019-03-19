6c462055bc0fa2efb6c99ceba63585fbc30db324
Rolando Moreno <rolando.moreno@ge.com>
2019-03-19 07:45:23 -0400

    cleaned up look a bit
    slept some and took another look and made some style changes

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     return (
         <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" /> 
-          <h2>{this.props.viewTitle}</h2>
+          <h4>{this.props.viewTitle}</h4>
         </header>
     );
   }
